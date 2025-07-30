bd6f6e877e615a5ab5a5e2d97f1a52f129f7d9be
Neumann Davila <74167448+neumann-davila@users.noreply.github.com>
2025-07-29 23:09:04 -0700

    Modifying header icons to move below profile when screen gets to small

--- src/objects/heading.tsx
+++ src/objects/heading.tsx
@@ -5,13 +5,15 @@ import profileImg from '../resources/profile.jpg';
 export function Heading() {
   return (
     <div id="heading">
-      <img src={profileImg} alt="Profile" id="profile-img"/>
-      <div id="profileInfo">
-        <div id='name'>
-          Neumann Davila
-        </div>
-        <div className="info">
-          ECE Undergradutate Student @ Virginia Tech
+      <div id="profile-group">
+        <img src={profileImg} alt="Profile" id="profile-img"/>
+        <div id="profileInfo">
+          <div id='name'>
+            Neumann Davila
+          </div>
+          <div className="info">
+            ECE Undergradutate Student @ Virginia Tech
+          </div>
         </div>
       </div>
       <IconGroup id="heading-icons"/>
